7ca091547159a3d8f1851b6e78289463f627f1fa
EAB227 <102165109+EAB227@users.noreply.github.com>
2023-06-22 23:39:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '杨金磊的个人网站', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '杨金磊的个人学习网站', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个正在学习的人，欢迎光临', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yjlgrwz。yjlgpt.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '杨金磊，博客', // 网站关键词 英文逗号隔开
